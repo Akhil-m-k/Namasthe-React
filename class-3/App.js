51ffd64d7945293b667ffd2034d8393bea7be675
Akhil <akhilmkrishnan2001@gmail.com>
2024-02-13 09:30:05 +0530

    "13-02-24, 09:30AM"

---
+++ class-3/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement('h1',{
+    id:"title",
+    style:{
+        color:"red",
+        backgroundColor:"green"
+    }
+},"Hello Akhil!");
+
+const heading2 = React.createElement('h2',// first argument is the tag name
+{ // second argument is attributes
+    id:"title",
+},
+"heading 2"); // third argument is the child elements
+
+// React.createElement => it gives an object => HTML DOM
+
+const container = React.createElement('div',{
+    id:"container"
+},[heading,heading2]);
+
+
+// react element is just an object 
+// console.log(heading);
+const root  = ReactDOM.createRoot(document.getElementById('root'));
+console.log("akhil");
+/// passing a react element inside the root
+root.render(container);
